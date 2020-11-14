3f2907c04fb0355b21258e69531639da5cf7aebd
Juandedios <juan.cruzd1@outlook.es>
2020-11-14 01:07:52 -0600

    descargar

--- app/js/service/dowloadfilejson.js
+++ app/js/service/dowloadfilejson.js
@@ -1,16 +1,49 @@
-function descargar(){
+function descargar() {
     ///trabajos usuarios
-    db.collection("trabajos").onSnapshot(function(trabajos) {    
-        trabajos.forEach(function(trabajo) {  
+    var obj =   [] ;
+    db.collection("trabajos").get().then( trabajos => {    
+        trabajos.forEach(function(trabajo) {   
             db.collection("cuentasusuarios").doc(trabajo.data().id_usuario_sol).get().then(function(usuario) {  
                 //console.log(trabajo.id, " => ", trabajo.data());
-                //console.log(usuario.id, " => ", usuario.data()); 
-                var json=$.extend(usuario.data(), trabajo.data());
-                console.log(json);
-            });
-        }); 
+                //console.log(usuario.id, " => ", usuario.data());  
+                //json_data=JSON.stringify(JSON.sort($.extend(usuario.data(), trabajo.data()))); 
+                obj.push(JSON.sort($.extend(usuario.data(), trabajo.data())));
+                //console.log(json_data);  
+                //console.log(Object.keys(json_data).map((key) => [key, json_data[key]]));
+            }); 
+        });  
+        
     }); 
+    var json = JSON.stringify(obj);
+    console.log(json);  
+    bajarfile(json); 
 
+}
+function bajarfile(obj){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(obj);
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href",     dataStr     );
+    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.click();
+}
+function isObject(v) {
+    return '[object Object]' === Object.prototype.toString.call(v);
+};
+JSON.sort = function(o) {
+if (Array.isArray(o)) {
+        return o.sort().map(JSON.sort);
+    } else if (isObject(o)) {
+        return Object
+            .keys(o)
+        .sort()
+            .reduce(function(a, k) {
+                a[k] = JSON.sort(o[k]);
+
+                return a;
+            }, {});
+    }
+
+    return o;
 }
 function json2array(json){
     var result = [];
