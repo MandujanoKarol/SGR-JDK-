be12c605db46328d420d6c7c079126bb026bb1b4
Juandedios <juan.cruzd1@outlook.es>
2020-11-13 23:15:14 -0600

    JSON

--- app/js/service/dowloadfilejson.js
+++ app/js/service/dowloadfilejson.js
@@ -4,14 +4,9 @@ function descargar(){
         trabajos.forEach(function(trabajo) {  
             db.collection("cuentasusuarios").doc(trabajo.data().id_usuario_sol).get().then(function(usuario) {  
                 //console.log(trabajo.id, " => ", trabajo.data());
-                //console.log(usuario.id, " => ", usuario.data());
-                var json_data =usuario.data();
-                var result = new Array(); // or the shortcut: = []
-                for(var i in json_data)
-                    result.push([i, json_data [i]]);
-                
-                //console.log(result);
-                console.log(json2array(usuario.data()));
+                //console.log(usuario.id, " => ", usuario.data()); 
+                var json=$.extend(usuario.data(), trabajo.data());
+                console.log(json);
             });
         }); 
     }); 
