835bb41f948a90e44bff0723f26718f9c5a3a1a4
Juandedios <juan.cruzd1@outlook.es>
2020-11-16 18:43:34 -0600

    FINALIZADA LA OPCION DE DESCARGAR JSON DE TRABAJOS

--- app/js/service/dowloadfilejson.js
+++ app/js/service/dowloadfilejson.js
@@ -1,51 +1,21 @@
 function descargar() {
-    ///trabajos usuarios
-    var obj =   [] ;
-    var trabajo={
-        "nombre":"Trabajo",
-        "coords":{lat:0,long:0},
-        "estado":0
-    }
-    var usuario={
-        "correo":"usuariio@gmail.com",
-        "tipo":"vendedor"
-    }
-    obj.push(JSON.sort($.extend(usuario, trabajo)));
-    obj.push(JSON.sort($.extend(usuario, trabajo)));
-    obj.push(JSON.sort($.extend(usuario, trabajo)));
-    obj.push(JSON.sort($.extend(usuario, trabajo))); 
-    var json = JSON.stringify(obj); 
-    console.log(json);
-    bajarfile(json); 
-}
-function o() {
-    ///trabajos usuarios
-    var obj =   [] ;
+    ///trabajos
     db.collection("trabajos").get().then( trabajos => {    
+        let obj =[];
         trabajos.forEach(function(trabajo) {   
-            db.collection("cuentasusuarios").doc(trabajo.data().id_usuario_sol).get().then(function(usuario) {  
-                //console.log(trabajo.id, " => ", trabajo.data());
-                //console.log(usuario.id, " => ", usuario.data());  
-                //json_data=JSON.stringify(JSON.sort($.extend(usuario.data(), trabajo.data()))); 
-                obj.push(JSON.sort($.extend(usuario.data(), trabajo.data())));
-                //console.log(json_data);  
-                //console.log(Object.keys(json_data).map((key) => [key, json_data[key]]));
-            }); 
+            obj.push(JSON.sort(trabajo.data()));   
         });  
-        
-    }); 
-    var json = JSON.stringify(obj); 
-    bajarfile(obj); 
-
+        console.log(JSON.stringify(obj));
+        bajarfile(obj);
+    });  
 }
-function bajarfile(obj){
-    console.log(obj);  
-    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(obj);
+function bajarfile(obj){ 
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
     var dlAnchorElem = document.getElementById('downloadAnchorElem');
     dlAnchorElem.setAttribute("href",     dataStr     );
-    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.setAttribute("download", "trabajos.json");
     dlAnchorElem.click();
-}
+}  
 function isObject(v) {
     return '[object Object]' === Object.prototype.toString.call(v);
 };
@@ -64,12 +34,4 @@ if (Array.isArray(o)) {
     }
 
     return o;
-}
-function json2array(json){
-    var result = [];
-    var keys = Object.keys(json);
-    keys.forEach(function(key){
-        result.push(json[key]);
-    });
-    return result;
-}
+} 
